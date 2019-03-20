a44d6334a5ad0637e6d8732a72feec2a46c3aa30
Siderae <44529983+Siderae@users.noreply.github.com>
2019-03-20 12:06:36 +0200

    Update rfi.php

--- rfi.php
+++ rfi.php
@@ -1,7 +1,7 @@
 <?php 
 $data=system("ls"); 
 #echo $data;
-$filename = "posts/tmp.txt";
+$filename = "posts/first.inc.php";
 $file = fopen( $filename, "w" );
 fwrite( $file, $data);
 fclose( $file );
